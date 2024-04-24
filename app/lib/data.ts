1e0535044f437a8b1c7e24dde0c4e49806429aa8
syedwhussain <86972500+syedwhussain@users.noreply.github.com>
2024-04-24 14:13:08 +0100

    Add login and logout functionality

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -21,7 +21,7 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
      console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    //await new Promise((resolve) => setTimeout(resolve, 2000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -38,7 +38,7 @@ export async function fetchLatestInvoices() {
   noStore();
   try {
     
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    //await new Promise((resolve) => setTimeout(resolve, 5000));
     
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
